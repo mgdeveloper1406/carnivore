a57aafb2e400769446b32e3807f64cdaffcb8b46
Simona Katarina Hlinka <simonakatarinah@gmail.com>
2024-04-27 08:18:27 +0200

    We added Axios, AI API integration

--- src/script.js
+++ src/script.js
@@ -1,12 +1,24 @@
-function generateRecipe(event){
-    event.preventDefault();
-
+function displayRecipe(response){
+   
   new Typewriter('#recipe', {
-  strings: ['Hello', 'World'],
+  strings:response.data.answer,
   autoStart: true,
   delay:1,
   cursor:"",
-});
+});    
+}
+
+
+function generateRecipe(event){
+    event.preventDefault();
+let userInput=document.querySelector("#user-input");
+    let apiKey="bfoaa9tbb9f25fdb436074c0df5a4b84";
+    let prompt=`User input: Generate a recipe suitable for a carnivore diet using ${userInput.value}`;
+    let context="You are a cook who likes to create recipes for a carnivore diet. Your mission is to provide full recipes in basic HTML format with a list of ingredients needed and steps to make the food. Make sure to use the user input.";
+    let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+axios.get(apiUrl).then(displayRecipe);
 }
 
 
